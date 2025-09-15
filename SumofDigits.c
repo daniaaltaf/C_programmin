5769e9ceb8b8ed9dd63886164166a9e13ce0fe1a
dania altaf <daniaaltaf10@gmail.com>
2025-09-15 22:36:56 +0530

    Create SumofDigits.c

---
+++ SumofDigits.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int num, temp, sum = 0, product = 1, digit;
+
+    scanf("%d", &num);
+
+    temp = num < 0 ? -num : num;
+
+    while (temp > 0) {
+        digit = temp % 10;
+        sum += digit;
+        product *= digit;
+        temp /= 10;
+    }
+
+    printf("Sum of digits: %d\n", sum);
+    printf("Product of digits: %d\n", product);
+
+    return 0;
+}
